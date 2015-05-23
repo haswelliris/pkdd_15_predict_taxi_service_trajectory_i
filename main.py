b65b1cc0b71450286622e7ba024f1c688fb5eb41
Shivan Sornarajah <sornars@sornars.com>
2015-05-23 01:48:12 +0100

    Initial submission.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from math import asin, atan2, cos, degrees, radians, sin, sqrt
 from mpl_toolkits.basemap import Basemap
+from sklearn import cross_validation, preprocessing
+from sklearn.ensemble import ExtraTreesRegressor
 
 def haversine(start, end):
     """
@@ -63,8 +65,11 @@ def process_df(df, train=True):
     df['WEEKDAY'] = df['TIMESTAMP'].dt.weekday
     df['POLYLINE'] = df['POLYLINE'].map(lambda x: json.loads(x))
     df['POLYLINE'] = df['POLYLINE'].map(lambda x: np.array(x))
-    df['KNOWN_TRIP_LENGTH'] = df['POLYLINE'].map(lambda x: len(x)) * 15
-    df.drop(df[df['KNOWN_TRIP_LENGTH'] < 30].index, inplace=True)
+    df['KNOWN_DURATION'] = df['POLYLINE'].map(lambda x: len(x)) * 15
+    
+    if train:
+        df.drop(df[df['KNOWN_DURATION'] < 30].index, inplace=True)
+    
     df['ORIGIN'] = df['POLYLINE'].map(lambda x: x[0])
     df['ORIGIN_LON'] = df['ORIGIN'].map(lambda x: x[0])
     df['ORIGIN_LAT'] = df['ORIGIN'].map(lambda x: x[1])
@@ -76,8 +81,8 @@ def process_df(df, train=True):
         df['DEST_LON'] = df['DEST'].map(lambda x: x[0])
         df['DEST_LAT'] = df['DEST'].map(lambda x: x[1])
         df['POLYLINE'] = df['POLYLINE'].map(lambda x: x[:-1])
-        df['KNOWN_TRIP_LENGTH'] = df['KNOWN_TRIP_LENGTH'].map(lambda x: x - 15)
-        df.drop(['DEST'], axis=1, inplace=True)
+        df['KNOWN_DURATION'] = df['KNOWN_DURATION'].map(lambda x: x - 15)
+        df.drop(['DEST', 'TRIP_ID'], axis=1, inplace=True)
         for column in ['ORIGIN_LON', 'ORIGIN_LAT', 'DEST_LON', 'DEST_LAT']:
             df = remove_outliers(df, column)
     
@@ -121,22 +126,67 @@ def explore(df):
     draw_map(df['ORIGIN_LON'], df['ORIGIN_LAT'])
     draw_map(df['DEST_LON'], df['DEST_LAT'], 2, 'Reds')
 
-def main():
+def main(explore=False):
     if os.path.isfile('./data/data.hdf'):
         print('Reading HDF')
-        df = pd.read_hdf('./data/data.hdf', 'train')
+        train_df = pd.read_hdf('./data/data.hdf', 'train')
+        test_df = pd.read_hdf('./data/data.hdf', 'test')
     else:
-        print('Reading CSV')
-        df = pd.read_csv('./data/train.csv')
-        print('Procesing DF')
-        df = process_df(df)
-        print('Saving to HDF')
-        df.to_hdf('./data/data.hdf', 'train')
+        print('Reading Train CSV')
+        train_df = pd.read_csv('./data/train.csv')
+        print('Procesing Train DF')
+        train_df = process_df(train_df)
+        print('Saving Train to HDF')
+        train_df.to_hdf('./data/data.hdf', 'train')
 
-    print('Mapping DF')
-    explore(df)
-    
-    return df
+        print('Reading Test CSV')
+        test_df = pd.read_csv('./data/test.csv')
+        print('Procesing Test DF')
+        test_df = process_df(test_df, False)
+        assert len(test_df) == 320
+        print('Saving Test to HDF')
+        test_df.to_hdf('./data/data.hdf', 'test')
+
+
+    if explore:
+        print('Mapping Train DF')
+        explore(train_df)
+
+    print('Training and running model')
+    scaler = preprocessing.StandardScaler()
+    train_data = train_df.drop(['DEST_LON', 'DEST_LAT'], axis=1).values
+    train_data = scaler.fit_transform(train_data)
+    target_data = np.array([train_df['DEST_LON'], train_df['DEST_LAT']]).T
+
+    train_data, cv_data, target_data, cv_target_data = cross_validation.train_test_split(
+        train_data, target_data, test_size=0.2)
+
+    clf = ExtraTreesRegressor(n_estimators=100)
+    clf.fit(train_data, target_data)
+    cv_predictions = clf.predict(cv_data)
+
+    results = []
+    for start, end in zip(cv_predictions, cv_target_data):
+        results.append(haversine(start, end))
+
+    print('Average estimate was {} km off.'.format(sum(results)/len(results)))
+
+    test_data = scaler.transform(test_df.drop(['TRIP_ID'], axis=1).values)
+
+    predictions = clf.predict(test_data)
+
+    with open('./data/output.csv', 'w') as o:
+        o.write('TRIP_ID,LATITUDE,LONGITUDE\n')
+        for trip_id, prediction in zip(test_df['TRIP_ID'], predictions):
+            o.write('{},{},{}\n'.format(trip_id, prediction[1], prediction[0]))
+
+    return locals()
+        
 
 if __name__ == '__main__':
-    df = main()
+    results = main()
+    
+
+
+
+
